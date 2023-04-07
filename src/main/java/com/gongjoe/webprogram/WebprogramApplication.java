ac3885d69d3d45b4f7873b7759f98e7031481c6a
GongJoe <3012374581@qq.com>
2023-04-07 10:02:56 +0800

    Initial commit

---
+++ src/main/java/com/gongjoe/webprogram/WebprogramApplication.java
@@ -0,0 +1,13 @@
+package com.gongjoe.webprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprogramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebprogramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gongjoe/webprogram/WebprogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongjoe.webprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprogramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
